86cdac93664eabc8f613cc652105974ba50bd9a7
Craig Harman <craig@craigharman.net>
2017-11-05 18:09:08 -0500

    Updated UI

---
+++ ui/js/cadet.js
@@ -0,0 +1,239 @@
+// The 'CADET' object provides methods for interacting with concrete-services
+//
+// None of the code in this file should manipulate the DOM.
+
+/* globals AnnotationTaskType, FeedbackServiceClient, FetchRequest,
+           FetchCommunicationServiceClient,
+           ResultsServerServiceClient, SearchClient, SearchQuery,
+           SearchProxyServiceClient, SearchType,
+           StoreCommunicationServiceClient, SummarizationServiceClient,
+           Thrift
+*/
+
+var CADET = {
+    // These variables are initialized by init()
+    feedback: undefined,
+    fetch: undefined,
+    search: undefined,
+    search_proxy: undefined,
+
+    defaultSearchProviders: {},
+    searchProvidersForSearchType: {},
+
+    /** Takes a SearchResult and FetchResult object.  For each
+     *  SearchResultItem object in SearchResult, add reference
+     *  variables:
+     *    - 'communication', which points to the Communication
+     *        identified by searchResultItem.communicationId
+     *    - 'sentence', which points to the Sentence identified
+     *        by searchResultItem.sentenceId
+     *
+     * @param {SearchResult} searchResult
+     * @param {FetchResult} fetchResult
+     */
+    addReferencesToSearchResultItems: function(searchResult, fetchResult) {
+        var comm;
+        var commIdToComm = {};
+        for (var i = 0; i < fetchResult.communications.length; i++) {
+            comm = fetchResult.communications[i];
+            commIdToComm[comm.id] = comm;
+            comm.addInternalReferences();
+        }
+        if (searchResult.searchResultItems) {
+            for (var j = 0; j < searchResult.searchResultItems.length; j++) {
+                var searchResultItem = searchResult.searchResultItems[j];
+                comm = commIdToComm[searchResultItem.communicationId];
+                searchResultItem.communication = comm;
+
+                if (comm) {
+                    // searchResultItem.sentence will be null if searchResultItem.sentenceId is not valid Sentence UUID
+                    searchResultItem.sentence = comm.getSentenceWithUUID(searchResultItem.sentenceId);
+                }
+            }
+        }
+    },
+
+    configureSearchProviders: function() {
+        function updateSearchProviderFromLocalStorage(searchTypeString, providers) {
+            if (localStorage.getItem('CADET.defaultSearchProviders.' + searchTypeString)) {
+                if (providers.includes(localStorage.getItem('CADET.defaultSearchProviders.' + searchTypeString))) {
+                    CADET.defaultSearchProviders[searchTypeString] = localStorage.getItem('CADET.defaultSearchProviders.' + searchTypeString);
+                }
+                else {
+                    localStorage.removeItem('CADET.defaultSearchProviders.' + searchTypeString);
+                }
+            }
+        }
+
+        var providers = CADET.search_proxy.getProviders();
+        var searchTypes = ['COMMUNICATIONS', 'ENTITY_MENTIONS', 'SENTENCES'];
+
+        for (var s = 0; s < searchTypes.length; s++) {
+            this.searchProvidersForSearchType[searchTypes[s]] = [];
+            updateSearchProviderFromLocalStorage(searchTypes[s], providers);
+        }
+
+        for (var i = 0; i < providers.length; i++) {
+            var capabilities = [];
+            try {
+                capabilities = CADET.search_proxy.getCapabilities(providers[i]);
+            }
+            catch (error) {
+                // TODO: Don't ignore error
+            }
+            for (var j = 0; j < capabilities.length; j++) {
+                for (var si = 0; si < searchTypes.length; si++) {
+                    if (capabilities[j].type === SearchType[searchTypes[si]]) {
+                        this.searchProvidersForSearchType[searchTypes[si]].push(providers[i]);
+
+                        if (!this.defaultSearchProviders[searchTypes[si]]) {
+                            this.defaultSearchProviders[searchTypes[si]] = providers[i];
+                        }
+                    }
+                }
+            }
+        }
+    },
+
+    /** Creates a SearchQuery object from a search string
+     * @param {String} searchString
+     * @returns {SearchQuery}
+     */
+    createSearchQueryFromSearchString: function(searchString, queryName) {
+        var i;
+
+        var q = new SearchQuery();
+        q.userId = this.userId;
+        // Example query:
+        // ?:"Where is brazil?" economics ?:who is the president of brazil" "dilma rousseff" ?:what is impeachment
+        q.rawQuery = searchString;
+        q.name = queryName;
+
+        // Question markup: ?:"______"
+        // To account for user error, any words found after ?: are considered part of a question until a closing quotation mark is reached.
+        // Matches: ["?:\"Where is brazil?\"", "?:who is the president of brazil\"", "?:what is impeachment"]
+        var questionMatches = searchString.match(/\?\:(\")?(([^\?\"])+(\s)*)+(\?)?(\")?/g);
+        q.questions = [];
+        // Filter out question markup (?:"") and add questions to SearchQuery.questions.
+        if (questionMatches){
+            for (i=0; i<questionMatches.length; i++){
+                var filtered = questionMatches[i].replace(/^[\?]/g,'').replace(/[:\"]/g, '');
+                q.questions.push(filtered);
+            }
+        }
+        // Remove any questions, leaving only potential terms.
+        // Example: "economics \"dilma rousseff\" "
+        var termFiltered = searchString.replace(/\?\:(\")?(([^\?\"])+(\s)*)+(\?)?(\")?/g, "");
+        // Match single words or phrases grouped with quotation marks.
+        var termMatches = termFiltered.match(/\"([^\"])+"|[^\s\"]+/g);
+        q.terms = [];
+        // Remove any empty elements and add terms to SearchQuery.terms.
+        if (termMatches){
+            for (i=0; i < termMatches.length; i++){
+                q.terms.push(termMatches[i].replace(/\"/g, ""));
+            }
+        }
+        q.type = SearchType.SENTENCES;
+
+        return q;
+    },
+
+    /** Takes a SearchResultItem and a FetchResult, returns the Communication
+     *  that is identified by the SearchResultItem and stored in the FetchResult.
+     *  Returns null if FetchResult did not contain the requested Communication
+     *
+     * @param {SearchResultItem} searchResultItem -
+     * @param {FetchResult} fetchResult - contains a list of Communications
+     * @returns {Communication|null}
+     */
+    getCommunicationForSearchResult: function(searchResultItem, fetchResult) {
+        for (var i = 0; i < fetchResult.communications.length; i++) {
+            if (fetchResult.communications[i].id === searchResultItem.communicationId) {
+                return fetchResult.communications[i];
+            }
+        }
+        return null;
+    },
+
+    /** Get list of Communication IDs stored in SearchResults
+     * @param {SearchResult} result
+     * @returns {List} - A list of Communication ID strings
+     */
+    getCommunicationIdListFromSearchResults: function(result) {
+        var idList = [];
+        // iterates over searchResultItems and adds communication IDs to idList
+        if (result.searchResultItems) {
+            for (var i = 0; i < result.searchResultItems.length; i++){
+                idList.push(result.searchResultItems[i].communicationId);
+            }
+        }
+        // returns a list of communication IDs
+        return idList;
+    },
+
+    /** Get human readable string describing SearchType
+     * @param {SearchType} searchType
+     * @returns {String}
+     */
+    getSearchTypeString: function(searchType) {
+        if (searchType === SearchType.COMMUNICATIONS) {
+            return 'COMMUNICATIONS';
+        }
+        else if (searchType === SearchType.SECTIONS) {
+            return 'SECTIONS';
+        }
+        else if (searchType === SearchType.SENTENCES) {
+            return 'SENTENCES';
+        }
+        else if (searchType === SearchType.ENTITIES) {
+            return 'ENTITIES';
+        }
+        else if (searchType === SearchType.ENTITY_MENTIONS) {
+            return 'ENTITY_MENTIONS';
+        }
+        else if (searchType === SearchType.SITUATIONS) {
+            return 'SITUATIONS';
+        }
+        else if (searchType === SearchType.SITUATION_MENTIONS) {
+            return 'SITUATION_MENTIONS';
+        }
+    },
+
+    init: function() {
+        var fetch_transport = new Thrift.Transport('/fetch_http_endpoint/');
+        var fetch_protocol = new Thrift.Protocol(fetch_transport);
+        this.fetch = new FetchCommunicationServiceClient(fetch_protocol);
+
+        var search_proxy_transport = new Thrift.Transport('/search_proxy_http_endpoint/');
+        var search_proxy_protocol = new Thrift.Protocol(search_proxy_transport);
+        this.search_proxy = new SearchProxyServiceClient(search_proxy_protocol);
+
+        this.configureSearchProviders();
+    },
+
+    /** Fetch Communications specified by Communication ID list
+     * @param {List} idList - A list of Communication ID strings
+     * @returns {FetchResult}
+     */
+    fetchComms: function(idList) {
+        // create args object to pass to FetchRequest
+        var args = {};
+        // list of communication IDs
+        args.communicationIds = idList;
+        // create FetchRequest object
+        var request = new FetchRequest(args);
+
+        // fetch the communications
+        var results = this.fetch.fetch(request);
+        return results;
+    },
+
+    /** Set default search provider for specified SearchType
+     * @param {String} searchTypeString - e.g. 'COMMUNICATIONS', 'ENTITY_MENTIONS'
+     * @param {String} providerName
+     */
+    setDefaultSearchProvider: function(searchTypeString, providerName) {
+        this.defaultSearchProviders[searchTypeString] = providerName;
+        localStorage.setItem('CADET.defaultSearchProviders.' + searchTypeString, providerName);
+    },
+};
---
+++ ui/js/index.js
@@ -0,0 +1,282 @@
+/* globals
+     CADET,
+     FetchRequest,
+     LRUMap,
+     SearchQuery,
+     SearchType
+*/
+
+// Global variables
+var SEARCH_RESULT_TABLE;
+var SEARCH_RESULT;
+var COMMS_MAP;
+
+function createResultsTable() {
+    SEARCH_RESULT_TABLE = $('#results_table').DataTable({
+        columns: [
+            {
+                title: 'Communication&nbsp;ID',
+                render: function(data, type, searchResultItem) {
+                    return searchResultItem.communicationId;
+                },
+                // Using a width of 1% causes Chrome, Firefox and Safari to render
+                // this column as narrow as possible while still being wide enough
+                // to accommodate the column's title.  This does not actually
+                // set the column width to be only 1% of the table width.  Without
+                // this styling, the width of this column was taking up over half
+                // of the width of the table when this column is shown by default.
+                width: '1%'
+            },
+            {
+                title: 'Sentence ID',
+                render: function(data, type, searchResultItem) {
+                    if (searchResultItem.sentenceId) {
+                        return searchResultItem.sentenceId.uuidString;
+                    }
+                    else {
+                        return '';
+                    }
+                },
+                visible: false
+            },
+            {
+                title: 'Score',
+                render: function(data, type, searchResultItem) {
+                    return searchResultItem.score;
+                },
+                visible: false
+            },
+            {
+                title: 'Text',
+                className: 'search_result_item_text',
+                render: function(data, type, searchResultItem) {
+                    if (type === 'display') {
+                        $.getJSON('/get_sentence_text',
+                                  {'communication_id': searchResultItem.communicationId,
+                                   'sentence_uuid_string': searchResultItem.sentenceId.uuidString},
+                                  function(data) {
+                                      $('#' + getIdForSearchResultItem(searchResultItem)).text(data.sentence_text);
+                                  });
+
+                        // Search result text will be updated when (asynchronous) getJSON() call [above] finishes
+                        return '<span id="' + getIdForSearchResultItem(searchResultItem) + '">';
+                    }
+                    else { // type !== 'display'
+                        return '';
+                    }
+                }
+            }
+        ],
+        createdRow: function(row, searchResultItem, index) {
+            // Add event handlers to DOM elements created by render()
+            //
+            // For more information about how to use DataTables' createdRow, see:
+            //   https://datatables.net/examples/advanced_init/row_callback.html
+
+            var searchResultItemId = SEARCH_RESULT.uuid.uuidString + '_' + index;
+            // TODO: Append 'search_result_' to DOM id
+            $('.search_result_item_text', row)
+                .attr('id', searchResultItem.communicationId)
+                .on('click',
+                    {
+                        'searchResultItem': searchResultItem,
+                        'searchResultItemId': searchResultItemId,
+                    },
+                    openSearchResultTab);
+        },
+
+        // The deferRender option allows "lazy loading" of Communications via fetch()
+        //   https://datatables.net/examples/ajax/defer_render.html
+        deferRender: true,
+
+        // The options for 'dom' are documented here:
+        //   https://datatables.net/reference/option/dom
+        // We are currently using the options:
+        //   r - processing display element
+        //   t - table
+        //   i - table information summary
+        //   p - pagination control
+        dom: 'rtip',
+        language: {
+            emptyTable: 'No matching search results'
+        },
+    });
+}
+
+function addResultToResultsTable(searchResult) {
+    SEARCH_RESULT = searchResult;
+    SEARCH_RESULT_TABLE.clear();
+    if (searchResult.searchResultItems.length > 0) {
+        for (var i = 0; i < searchResult.searchResultItems.length; i++) {
+            // We add an index to each SearchResultItem so that
+            // getIdForSearchResultItem() will return a unique DOM ID
+            // for the <span> for each SearchResultItem
+            searchResult.searchResultItems[i].search_result_index = i;
+        }
+        SEARCH_RESULT_TABLE.rows.add(searchResult.searchResultItems);
+    }
+    SEARCH_RESULT_TABLE.draw();
+}
+
+function displayAndThrowError(error) {
+    displayErrorMessage(error.message);
+    throw error;
+}
+
+function displayErrorMessage(message) {
+    $('#errors').html('<div class="alert alert-danger" role="alert">' + message + '</div>');
+}
+
+function executeSearchQuery(searchQuery) {
+    // removes any previous error messages
+    $('#errors').empty();
+
+    if (CADET.defaultSearchProviders[CADET.getSearchTypeString(searchQuery.type)] === undefined) {
+        displayErrorMessage('No search provider registered for search type ' +
+                            CADET.getSearchTypeString(searchQuery.type));
+        return;
+    }
+
+    var searchProvider;
+    if (searchQuery.type === SearchType.COMMUNICATIONS) {
+        searchProvider = CADET.defaultSearchProviders.COMMUNICATIONS;
+    }
+    else if (searchQuery.type === SearchType.SENTENCES) {
+        searchProvider = CADET.defaultSearchProviders.SENTENCES;
+    }
+
+    CADET.search_proxy.search(searchQuery, searchProvider, true)
+        .done(function(searchResult) {
+            addResultToResultsTable(searchResult);
+
+            $('a[href="#search_results"]').tab('show');
+        });
+}
+
+function executeSearchQueryEventHandler(event) {
+    executeSearchQuery(event.data.searchQuery);
+}
+
+function executeSearchQueryFromSearchBox() {
+    var searchInput = document.getElementById('user_search').value;
+
+    executeSearchQuery(CADET.createSearchQueryFromSearchString(searchInput, 'TODO: IGNORED'));
+}
+
+function getIdForSearchResultItem(searchResultItem) {
+    return(selectorSafeString('' + searchResultItem.communicationId + '_' +
+                              searchResultItem.sentenceId.uuidString + '_' +
+                              searchResultItem.search_result_index));
+}
+
+/** Create a new tab containing the Communication text for a search result
+ */
+function openSearchResultTab(event) {
+    var searchResultItem = event.data.searchResultItem;
+    var searchResultItemId = event.data.searchResultItemId;
+
+    // Add tab with Communication ID as title and an 'X' button for closing the tab
+    $('#nav-tabs').append(
+        $('<li>').append(
+            $('<a>').attr('data-toggle', 'tab')
+                    .attr('href', '#' + searchResultItemId)
+                    .text(searchResultItem.communicationId)
+                    .append(
+                        // We are embedding the <span> with the remove ('X') link *in* the
+                        // <a> link because of Bootstrap CSS rules that use the selectors
+                        // ".nav-tabs>li>a" and ".nav>li>a".
+                        $('<span>')
+                            .addClass('glyphicon glyphicon-remove')
+                            .css('margin-left', '0.5em')
+                            .on('click', {'searchResultItemId': searchResultItemId}, function(event) {
+                                // Remove the <div> with the tab content
+                                $('#' + event.data.searchResultItemId).remove();
+                                // Remove the tab
+                                $(this).closest('li').remove();
+                                // Show the Search Results tab
+                                $('a[href="#search_results"]').tab('show');
+                            }))));
+
+    $('#tab-content').append('<div id="' + searchResultItemId +'" class="tab-pane"></div>');
+
+    var request = new FetchRequest();
+    request.communicationIds = [searchResultItem.communicationId];
+    CADET.fetch.fetch(request, true).done(function(fetchResult) {
+        if (fetchResult.communications && fetchResult.communications.length === 1) {
+            // Add content to <div> for the newly created tab
+            $('div #' + searchResultItemId).append(
+                $('<div>').communicationWidget(fetchResult.communications[0]));
+
+            $('a[href="#'+ searchResultItemId +'"]').tab('show');
+        }
+        else {
+            displayErrorMessage('Unable to find Communication with ID "' + request.communicationIds[0] + '"');
+        }
+    });
+}
+
+
+/**
+ * Takes a string, returns a version of the string that replaces
+ * any of the CSS selector metacharacters:
+ *   !"#$%&'()*+,./:;<=>?@[\]^`{|}~
+ * with an underscore.  Per the jQuery documentation, these
+ * metacharacters in CSS selector names if they are escaped with '\\',
+ * but replacing them with underscores seems less likely to cause
+ * strange behavior.
+ *
+ * Useful for handling Entity IDs that are prefixed with a colon,
+ * e.g. ':Entity_ENG_EDL_0088070'.
+ *
+ * @param {String} s
+ * @returns {String}
+ */
+function selectorSafeString(s) {
+  return s.replace(/[!"#$%&'()*+,./:;<=>?@[\]^`{|}~]/g, '_');
+}
+
+
+function updateServiceStatus() {
+    var services = ['search_proxy', 'fetch'];
+    var serviceStatusMessage = '';
+    var unavailableServices = [];
+
+    for (var index in services) {
+        try {
+            if (!CADET[services[index]].alive()) {
+                unavailableServices.push(services[index]);
+                serviceStatusMessage += '<li>The ' + services[index] + ' service is not alive</li>';
+            }
+        }
+        catch (error) {
+            unavailableServices.push(services[index]);
+            serviceStatusMessage += '<li>Error when trying to connect to ' + services[index] + ' service: ' +
+                                    error.name + ' - ' + error.message +
+                                    '</li>';
+        }
+    }
+    if (unavailableServices.length > 0) {
+        displayErrorMessage(serviceStatusMessage);
+    }
+}
+
+// initialize all CADET clients
+CADET.init();
+COMMS_MAP = new LRUMap(35);
+
+$(document).ready(function() {
+    updateServiceStatus();
+
+    // search box is focused on pageload
+    $('#user_search').focus();
+
+    // press enter key to trigger search button click handler
+    $('#user_search, #query_name').bind('keypress', function(e) {
+        if (e.keyCode===13) {
+            executeSearchQueryFromSearchBox();
+        }
+    });
+    createResultsTable();
+
+    $('#search_button').on('click', executeSearchQueryFromSearchBox);
+});
